430982fa59298f559372f9aea00c00bdd489a258
Sam Mikes <smikes@cubane.com>
2014-04-15 19:53:52 +0100

    test: new tests for SDFTransform

--- test/stream.js
+++ test/stream.js
@@ -17,6 +17,22 @@ function spySplitter(expectedCount, done) {
     return splitter;
 }
 
+function spyTransform(expectedCount, done) {
+    var callback = sinon.spy(),
+        transform = new parser.SDFTransform();
+
+    transform.on('finish', function () {
+        (callback.callCount).should.be.exactly(expectedCount);
+
+        done();
+    }).on('data', function (chunk) {
+        callback(chunk);
+    });
+
+    return transform;
+}
+
+
 describe('streaming', function () {
     describe('splitter', function () {
         it('should be able to open a stream on the fixtures (single)', function (done) {
@@ -58,4 +74,49 @@ describe('streaming', function () {
                 .pipe(splitter);
         });
     });
+
+    describe('transform', function () {
+        it('should pass the molfile contents to the callback', function (done) {
+            var callback = sinon.spy(),
+                transform = new parser.SDFTransform(callback);
+
+            transform.on('data', function (chunk) {
+                callback(String(chunk));
+            }).on('finish', function () {
+                callback.calledWithExactly('foo\n');
+                callback.calledWithExactly('bar\n');
+                callback.calledWithExactly('bletch\n');
+                done();
+            });
+
+            resumer().queue('foo\n$$$$\nbar\n$$$$\nbletch\n$$$$').end()
+                .pipe(transform);
+
+        });
+        it('should pass the multiline contents reasonably', function (done) {
+            var callback = sinon.spy(),
+                transform = new parser.SDFTransform(callback),
+                header = "\n  Mol2Comp06180618072D\n\n";
+
+            transform.on('data', function (chunk) {
+                callback(String(chunk));
+            }).on('finish', function () {
+                callback.calledWithExactly(header);
+                done();
+            });
+
+            resumer().queue(header + '$$$$\n').end()
+                .pipe(transform);
+
+        });
+
+
+        it('should be able to open a stream on the fixtures (single)', function (done) {
+            var t = spyTransform(1, done);
+
+            fs.createReadStream('test/fixtures/single.sdf')
+                .pipe(t);
+        });
+    });
+
 });
