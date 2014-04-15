25603e5ec80c778d4b86bc5061ffe0be4faa406f
smikes <smikes@cubane.com>
2014-04-15 10:52:35 -0600

    add generated docs

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "molfile"
+    ],
+    "modules": [
+        "molfile"
+    ],
+    "allModules": [
+        {
+            "displayName": "molfile",
+            "name": "molfile",
+            "description": "Parser for MDL/Symyx CTfile formats\n\nUsage:\n    var molfile = require('molfile');\n\n    var parsed = molfile.parseMol(string);"
+        }
+    ]
+} };
+});
